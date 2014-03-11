21347b05064417e34513ad5453ed208141995188
Samir Undavia <samir1@brandeis.edu>
2014-03-11 01:41:58 -0400

    initial commit

---
+++ app/models/item.rb
@@ -0,0 +1,3 @@
+class Item < ActiveRecord::Base
+	self.inheritance_column = nil
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lostandfound::Application.config.secret_key_base = '644794c639c42b6939fe494e6f128fe0f4c624261e9df426b504c0ac3f39b76aefb78cb30a64fd09520601e84349f417a45e4ab9ec8da0c0dcc8af8af27d5511'
